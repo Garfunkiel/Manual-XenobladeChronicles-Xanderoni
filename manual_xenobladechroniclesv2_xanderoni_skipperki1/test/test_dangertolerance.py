4f290cda4f19ea885c6367f22296697d9ffe662e
Garfunkiel <Garfunkiel@users.noreply.github.com>
2026-07-04 18:57:24 -0500

    Added a danger tolerance option to move Unique Monsters earlier/later in logic

---
+++ manual_xenobladechroniclesv2_xanderoni_skipperki1/test/test_dangertolerance.py
@@ -0,0 +1,46 @@
+from ..Game import game_name
+from .manual_test import XenobladeManualTest
+
+class XenobladeManualTest_DangerTolerance_Negative10(XenobladeManualTest):
+    game = game_name
+    options = {
+        "Danger_Tolerance": -10
+    }
+
+    def test_DangerTolerance_GentleRodriguez(self):
+        self.checkItemsForLocation("Gentle Rodriguez (Lv 40)",
+            [ "Progressive Hunting License:10" ] + self.getRegionKeyRequirements("Sword Valley")
+        )
+
+class XenobladeManualTest_DangerTolerance_0(XenobladeManualTest):
+    game = game_name
+    options = {
+        "Danger_Tolerance": 0
+    }
+
+    def test_DangerTolerance_GentleRodriguez(self):
+        self.checkItemsForLocation("Gentle Rodriguez (Lv 40)",
+            [ "Progressive Hunting License:1" ] + self.getRegionKeyRequirements("Prison Island (1st Visit)")
+        )
+
+class XenobladeManualTest_DangerTolerance_35(XenobladeManualTest):
+    game = game_name
+    options = {
+        "Danger_Tolerance": 35
+    }
+
+    def test_DangerTolerance_GentleRodriguez(self):
+        self.checkItemsForLocation("Gentle Rodriguez (Lv 40)",
+            [ "Progressive Hunting License:1" ] + self.getRegionKeyRequirements("Tephra Cave")
+        )
+
+class XenobladeManualTest_DangerTolerance_119(XenobladeManualTest):
+    game = game_name
+    options = {
+        "Danger_Tolerance": 119
+    }
+
+    def test_DangerTolerance_GentleRodriguez(self):
+        self.checkItemsForLocation("Gentle Rodriguez (Lv 40)",
+            [ "Progressive Hunting License:1" ] + self.getRegionKeyRequirements("Colony 9")
+        )
