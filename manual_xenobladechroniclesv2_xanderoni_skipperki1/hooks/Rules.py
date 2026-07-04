217ba2a06b9d37619da4d5b0305b31adcc73f94e
Garfunkiel <Garfunkiel@users.noreply.github.com>
2026-07-04 18:57:24 -0500

    Added a danger tolerance option to move Unique Monsters earlier/later in logic

--- manual_xenobladechroniclesv2_xanderoni_skipperki1/hooks/Rules.py
+++ manual_xenobladechroniclesv2_xanderoni_skipperki1/hooks/Rules.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from worlds.AutoWorld import World
-from ..Helpers import clamp, get_items_with_value
+from ..Helpers import clamp, get_items_with_value, get_option_value
 from BaseClasses import MultiWorld, CollectionState
 
 import re
@@ -36,3 +36,48 @@ def questPaolaAndNarineReq():
                 " OR (|Melia Progressive Affinity Rank:4| AND |Fiora Progressive Affinity Rank:4|)" \
                 " OR (|Melia Progressive Affinity Rank:4| AND |Seven Progressive Affinity Rank:4|)" \
                 ")"
+
+REGION_LEVELS = [
+    {"region": "Colony 9",                      "level":  1, "requires": ""},
+    {"region": "Tephra Cave",                   "level": 12, "requires": "|Tephra Cave Key:1|"},
+    {"region": "Bionis' Leg",                   "level": 25, "requires": "|Bionis' Leg Key:1|"},
+    {"region": "Colony 6",                      "level": 26, "requires": "|Colony 6 Key:1|"},
+    {"region": "Ether Mine",                    "level": 27, "requires": "|Ether Mine Key:1|"},
+    {"region": "Satorl Marsh",                  "level": 28, "requires": "|Satorl Marsh Key:1|"},
+    {"region": "Bionis' Interior (1st Visit)",  "level": 32, "requires": "|Bionis' Interior (1st Visit) Key:1|"},
+    {"region": "Makna Forest",                  "level": 34, "requires": "|Makna Forest Key:1|"},
+    {"region": "Frontier Village",              "level": 36, "requires": "|Frontier Village Key:1|"},
+    {"region": "Eryth Sea",                     "level": 37, "requires": "|Eryth Sea Key:1|"},
+    {"region": "Alcamoth",                      "level": 37, "requires": "|Alcamoth Key:1|"},
+    {"region": "High Entia Tomb",               "level": 38, "requires": "|High Entia Tomb Key:1|"},
+    {"region": "Prison Island (1st Visit)",     "level": 42, "requires": "|Prison Island (1st Visit) Key:2|"},
+    {"region": "Valak Mountain",                "level": 48, "requires": "|Valak Mountain Key:2|"},
+    {"region": "Sword Valley (MISSABLE)",       "level": 52, "requires": "|Sword Valley Key:2| AND |Progressive Hunting License:10|"},
+    {"region": "Galahad Fortress (MISSABLE)",   "level": 55, "requires": "|Galahad Fortress Key:2|"},
+    {"region": "Fallen Arm",                    "level": 58, "requires": "|Fallen Arm Key:2|"},
+    {"region": "Mechonis Field (MISSABLE)",     "level": 60, "requires": "|Mechonis Field Key:2|"},
+    {"region": "Central Factory (MISSABLE)",    "level": 65, "requires": "|Central Factory Key:2|"},
+    {"region": "Agniratha (MISSABLE)",          "level": 70, "requires": "|Agniratha Key:3|"},
+    {"region": "Mechonis Core",                 "level": 72, "requires": "|Mechonis Core Key:3| AND |Progressive Hunting License:16|"},
+    {"region": "Bionis' Interior (2nd Visit)",  "level": 75, "requires": "|Bionis' Interior (2nd Visit) Key:3|"},
+    {"region": "Prison Island (2nd Visit)",     "level": 80, "requires": "|Prison Island (2nd Visit) Key:3|"},
+    {"region": "Memory Space",                  "level": 82, "requires": "|Memory Fragment:20|"}
+]
+
+def hasDangerTolerance(multiworld: MultiWorld, player: int, monsterLevel: int):
+    DT = get_option_value(multiworld, player, "Danger_Tolerance")
+    effectiveLevel = monsterLevel - DT
+
+    requirements = ""
+
+    for region in REGION_LEVELS:
+        if requirements != "":
+            requirements += " AND "
+        requirements += region["requires"]
+
+        if effectiveLevel < region["level"]:
+            break
+
+    if requirements != "":
+        return requirements
+    return True
